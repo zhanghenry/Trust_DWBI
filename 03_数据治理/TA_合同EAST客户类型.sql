c27a4cfe1522abe64e594b1347b96b0e6bdd5594
zhanghenry <zhangtianfeng@ztf.local>
2017-01-17 11:13:29 +0800

    增加非银机构east客户类型判断

--- 03_数据治理/TA_合同EAST客户类型.sql
+++ 03_数据治理/TA_合同EAST客户类型.sql
@@ -15,3 +15,19 @@ select b.c_tano         as 理财账号,
           from ttrustcontractdetails c
          where a.l_serialno = c.l_serialno
            and c.c_eastcusttype = '0');
+
+--非银行委托人（名字包含银行或合作社的），资金来源是银行理财或银行自有的		   
+select b.c_tano         as 理财账号,
+       b.c_clientname   as 委托人姓名,
+       b.c_clientkind   as 委托人类型,
+       b.c_idtype       as 证件类型,
+       b.c_idcard       as 证件号码,
+       a.l_serialno     as 合同序列号,
+       a.c_eastcusttype as east客户类型--0：普通机构,非金融机构和各类基金性质资金
+  from ttrustcontractdetails a, hstdc.cm_client b
+ where a.c_fundacco = b.c_tano
+   and (b.c_clientname not like '%银行%' and b.c_clientname not like '%合作%社%' )
+   and exists (select 1
+          from ttrustcontractdetails c
+         where a.l_serialno = c.l_serialno
+           and c.c_eastcusttype in ('2','3'));
