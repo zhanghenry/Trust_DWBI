4fd1827d659099ce62b4cb198ecab58cab1c2195
zhanghenry <zhangtianfeng@ztf.local>
2017-01-17 10:45:27 +0800

    脚本初始化

---
+++ 03_数据治理/TA_合同EAST客户类型.sql
@@ -0,0 +1,17 @@
+
+--针对信托合同中的c_eastcusttype字段进行数据体检；
+--如果委托人实际为银行等金融机构，则EAST客户类型原则上不应该为0，即非金融机构；
+select b.c_tano         as 理财账号,
+       b.c_clientname   as 委托人姓名,
+       b.c_clientkind   as 委托人类型,
+       b.c_idtype       as 证件类型,
+       b.c_idcard       as 证件号码,
+       a.l_serialno     as 合同序列号,
+       a.c_eastcusttype as east客户类型--0：普通机构,非金融机构和各类基金性质资金
+  from ttrustcontractdetails a, hstdc.cm_client b
+ where a.c_fundacco = b.c_tano
+   and (b.c_clientname like '%银行%' or b.c_clientname like '%基金%' or b.c_clientname like '%保险%' or b.c_clientname like '%信托%')
+   and exists (select 1
+          from ttrustcontractdetails c
+         where a.l_serialno = c.l_serialno
+           and c.c_eastcusttype = '0');
