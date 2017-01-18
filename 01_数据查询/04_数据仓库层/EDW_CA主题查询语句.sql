c4c3f31ea7fdb277c95f27ab3a36a458da65da39
zhanghenry <zhangtianfeng@ztf.local>
2017-01-18 16:38:45 +0800

    增加个人机构特征查询，整理查询语句

--- 01_数据查询/04_数据仓库层/EDW_CA主题查询语句.sql
+++ 01_数据查询/04_数据仓库层/EDW_CA主题查询语句.sql
@@ -1,3 +1,48 @@
+--客户结构走势--按客户类别和客户类型
+select b.c_cust_cate_n, b.c_cust_type_n, count(a.f_count_agg)
+  from tt_ca_count_cust_m a, dim_ca_customer b, dim_ca_behavior c
+ where a.l_cust_id = b.l_cust_id
+   and a.l_beha_id = c.l_beha_id
+   and c.c_beha_code = 'KH_XX_SS_CJ'
+   and a.l_month_id = 201612
+   and b.l_effective_flag = 1
+ group by b.c_cust_cate_n, b.c_cust_type_n;
+
+--事实个人客户特征分析
+--可根据dim_pb_individual的属性进行调整
+select d.l_age, count(1)
+  from tt_ca_count_cust_m a,
+       dim_ca_customer    b,
+       dim_ca_behavior    c,
+       dim_pb_individual  d
+ where a.l_cust_id = b.l_cust_id
+   and a.l_beha_id = c.l_beha_id
+   and c.c_beha_code = 'KH_XX_SS_CJ'
+   and a.l_month_id = 201612
+   and b.l_effective_flag = 1
+   and b.c_cust_type = '1'
+   and b.c_cust_cate = 'A'
+   and b.l_unique_id = d.l_indiv_id
+ group by d.l_age; 
+ 
+--事实机构客户特征分析
+--可根据dim_pb_organization的属性进行调整
+select d.c_organ_type_n, count(1)
+  from tt_ca_count_cust_m  a,
+       dim_ca_customer     b,
+       dim_ca_behavior     c,
+       dim_pb_organization d
+ where a.l_cust_id = b.l_cust_id
+   and a.l_beha_id = c.l_beha_id
+   and c.c_beha_code = 'KH_XX_SS_CJ'
+   and a.l_month_id = 201612
+   and b.l_effective_flag = 1
+   and b.c_cust_type = '2'
+   and b.c_cust_cate = 'A'
+   and b.l_unique_id = d.l_organ_id
+ group by d.c_organ_type_n;
+
+ 
 --总客户数
 select t.c_cust_type_n, count(*)
   from dim_ca_customer t
@@ -8,14 +53,11 @@ select t.c_cust_type_n, count(*)
 --会员人数
 select count(*) from dim_ca_member t where t.l_effective_flag = 1;
 
---经纪人下面客户数量
-select a.c_broker_code, a.c_broker_name, count(*)
-  from dim_ca_broker a, dim_ca_broker_cust b, dim_ca_customer c
- where a.l_broker_id = b.l_broker_id
-   and b.l_cust_id = c.l_cust_id
-   and b.c_rela_type = '0' --客户经理
- group by a.c_broker_code, a.c_broker_name
- order by count(*) desc;
+--经纪人团队下面的经纪人
+select a.c_team_code, a.c_team_name, b.c_broker_code, b.c_broker_name
+  from dim_ca_broker_team a, dim_ca_broker b
+ where a.l_team_id = b.l_team_id
+   and b.l_effective_flag = 1;
 
 --经纪团队
 select b.c_area_name,
@@ -29,18 +71,17 @@ select b.c_area_name,
  where a.l_area_id = b.l_area_id
    and a.l_team_id = c.l_team_id;
 
---客户持有规模
-select * from dim_ca_behavior;
-select * from dim_ca_broker_cust;
-select c.c_cust_type_n,b.c_beha_name,sum(a.f_share_agg)/100000000
-  from tt_ca_share_cust_m a, dim_ca_behavior b,dim_ca_customer c
- where a.l_beha_id = b.l_beha_id
-   and a.l_cust_id = c.l_cust_id
-   --and b.c_beha_code = 'KH_JY_SH'
-   and a.l_month_id = 201611
-   group by c.c_cust_type_n,b.c_beha_name;
+--经纪人下面客户数量
+select a.c_broker_code, a.c_broker_name, count(*)
+  from dim_ca_broker a, dim_ca_broker_cust b, dim_ca_customer c
+ where a.l_broker_id = b.l_broker_id
+   and b.l_cust_id = c.l_cust_id
+   and b.c_rela_type = '0' --客户经理
+ group by a.c_broker_code, a.c_broker_name
+ order by count(*) desc;
 
---客户持有规模--按客户类型
+--客户份额情况--按客户类型
+--可根据行为ID进行调整
 select c.c_cust_type_n, sum(a.f_share_agg) / 100000000
   from tt_ca_share_cust_m a, dim_ca_behavior b, dim_ca_customer c
  where a.l_beha_id = b.l_beha_id
@@ -50,9 +91,14 @@ select c.c_cust_type_n, sum(a.f_share_agg) / 100000000
    and c.c_cust_cate = 'A' --事实客户
  group by c.c_cust_type_n;
 
---客户经理对应客户持有份额
-select e.c_broker_name,sum(a.f_share_agg)
-  from tt_ca_share_cust_m a, dim_ca_behavior b,dim_ca_customer c,dim_ca_broker_cust d,dim_ca_broker e
+--客户经理对应客户份额情况
+--可根据行为ID进行调整
+select e.c_broker_name, sum(a.f_share_agg)
+  from tt_ca_share_cust_m a,
+       dim_ca_behavior    b,
+       dim_ca_customer    c,
+       dim_ca_broker_cust d,
+       dim_ca_broker      e
  where a.l_beha_id = b.l_beha_id
    and a.l_cust_id = c.l_cust_id
    and c.l_cust_id = d.l_cust_id
@@ -60,11 +106,18 @@ select e.c_broker_name,sum(a.f_share_agg)
    and d.l_broker_id = e.l_broker_id
    and b.c_beha_code = 'KH_JY_CC'
    and a.l_month_id = 201612
-   group by e.c_broker_name;
+ group by e.c_broker_name
+ order by sum(a.f_share_agg) desc;
    
---财富部门对应客户的持有份额
-select f.c_team_name,sum(a.f_share_agg)
-  from tt_ca_share_cust_m a, dim_ca_behavior b,dim_ca_customer c,dim_ca_broker_cust d,dim_ca_broker e,dim_ca_broker_team f
+--财富部门对应客户份额情况
+--可根据行为ID进行调整
+select f.c_team_name, sum(a.f_share_agg)
+  from tt_ca_share_cust_m a,
+       dim_ca_behavior    b,
+       dim_ca_customer    c,
+       dim_ca_broker_cust d,
+       dim_ca_broker      e,
+       dim_ca_broker_team f
  where a.l_beha_id = b.l_beha_id
    and a.l_cust_id = c.l_cust_id
    and c.l_cust_id = d.l_cust_id
@@ -73,13 +126,8 @@ select f.c_team_name,sum(a.f_share_agg)
    and e.l_team_id = f.l_team_id
    and b.c_beha_code = 'KH_JY_CC'
    and a.l_month_id = 201612
-   group by f.c_team_name;
-
---经纪人团队下面的经纪人
-select a.c_team_code, a.c_team_name, b.c_broker_code, b.c_broker_name
-  from dim_ca_broker_team a, dim_ca_broker b
- where a.l_team_id = b.l_team_id
-   and b.l_effective_flag = 1;
+ group by f.c_team_name
+ order by sum(a.f_share_agg) desc;
 
 --客户人数
 select a.l_month_id,
