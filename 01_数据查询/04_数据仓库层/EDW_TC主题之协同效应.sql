5276d3212e00525646fa1e0879b0732e2a83021c
zhanghenry <zhangtianfeng@ztf.local>
2017-02-08 13:56:26 +0800

    语句调整，判断资金来源的顺序有问题

--- 01_数据查询/04_数据仓库层/EDW_TC主题之协同效应.sql
+++ 01_数据查询/04_数据仓库层/EDW_TC主题之协同效应.sql
@@ -17,31 +17,35 @@ having sum(t.f_balance_eot) <> 0;
 
 --分行实际收入
 with temp_fdsrc as
- (select row_number() over(partition by c.c_proj_code order by b.c_fdsrc_code,
-		case when b.c_fdsrc_code like '11%' and d.c_inst_name = '交通银行' then '1直投' 
-			 when b.c_fdsrc_code like '12%' and d.c_inst_name = '交通银行' then '2理财'
-			 else '3其他' end desc) as l_rn,
-         c.c_proj_code,
-         case when b.c_fdsrc_code like '11%' and d.c_inst_name = '交通银行' then '1直投'
-			  when b.c_fdsrc_code like '12%' and d.c_inst_name = '交通银行' then '2理财'
-           else '3其他' end as c_fdsrc_name
-    from tt_tc_fdsrc_proj_m   a,
-         dim_tc_fund_source   b,
-         dim_pb_project_basic c,
-         dim_pb_institution   d
-   where a.l_fdsrc_id = b.l_fdsrc_id(+)
-     and a.l_proj_id = c.l_proj_id
-     and a.l_fdsrc_inst_id = d.l_inst_id(+)
-     and a.l_month_id = 201610 --and c.c_proj_code = 'E701'
-  ),
+ (select row_number() over(partition by t.c_proj_code order by t.c_fdsrc_name) as l_rn,
+         t.c_proj_code,
+         t.c_fdsrc_name
+    from (select c.c_proj_code,
+                 case
+                   when b.c_fdsrc_code like '11%' and d.c_inst_name = '交通银行' then
+                    '1直投'
+                   when b.c_fdsrc_code like '12%' and d.c_inst_name = '交通银行' then
+                    '2理财'
+                   else
+                    '3其他'
+                 end as c_fdsrc_name
+            from tt_tc_fdsrc_proj_m   a,
+                 dim_tc_fund_source   b,
+                 dim_pb_project_basic c,
+                 dim_pb_institution   d
+           where a.l_fdsrc_id = b.l_fdsrc_id(+)
+             and a.l_proj_id = c.l_proj_id
+             and a.l_fdsrc_inst_id = d.l_inst_id(+)
+             and a.l_month_id = 201701 --and c.c_proj_code = 'G144'
+			 ) t),
 temp_actual_year as
  (select b.c_proj_code, sum(a.f_actual_tot) as f_actual_year
     from tt_sr_tstrev_proj_m a, dim_pb_project_basic b
    where a.l_proj_id = b.l_proj_id
      and a.l_month_id >= substr(b.l_effective_date, 1, 6)
      and a.l_month_id < substr(b.l_expiration_date, 1, 6)
-     and substr(a.l_month_id, 1, 4) = 2016
-     and a.l_month_Id <= 201610 --and b.c_proj_code = 'E701'
+     and substr(a.l_month_id, 1, 4) = 2017
+     and a.l_month_Id <= 201701 --and b.c_proj_code = 'E701'
    group by b.c_proj_code, b.c_proj_name)
 select d.c_fdsrc_name,
        c.c_inst_name,
@@ -63,3 +67,4 @@ select d.c_fdsrc_name,
 --and a.c_proj_code = 'E701'
  group by d.c_fdsrc_name, c.c_inst_name, a.c_proj_code, a.c_proj_name
  order by d.c_fdsrc_name, c.c_inst_name, a.c_proj_code, a.c_proj_name;
+
