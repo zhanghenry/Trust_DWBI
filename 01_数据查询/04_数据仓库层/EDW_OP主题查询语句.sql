7cf4f30ce2d11033ad0d6da35f4a7a6bfd47692a
zhanghenry <zhangtianfeng@ztf.local>
2017-03-03 15:48:39 +0800

    增加损益项目表查询语句

--- 01_数据查询/04_数据仓库层/EDW_OP主题查询语句.sql
+++ 01_数据查询/04_数据仓库层/EDW_OP主题查询语句.sql
@@ -8,6 +8,19 @@ select a.c_indic_code,
  where a.l_indic_id = b.l_indic_id
    and b.l_month_id = 201611;
 
+--自定义报告-损益项目表
+--民生在用  
+select a.c_item_code,
+       a.c_item_name,
+       c.f_value1    as 实际,
+       c.f_value2    as 预测,
+       c.f_value3    as 预算
+  from dim_op_report_item a, dim_op_report b, tt_op_report_m c
+ where a.l_report_id = b.l_report_id
+   and a.l_item_id = c.l_item_id
+   and b.c_report_code = 'SYXMB'
+   and c.l_month_id = 201612;  
+   
 --自定义报告-固定费用执行情况
 --长安在用
 select a.c_item_code, a.c_item_name, c.f_value1 as 实际, c.f_value2 as 预计
