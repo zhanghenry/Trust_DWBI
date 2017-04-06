a7953f91b1fa47c3feee891b72797660124a5a64
zhanghenry <zhangtianfeng@ztf.local>
2017-04-06 16:28:38 +0800

    语句更新
    增加项目属性用于综合化口径筛选

--- 01_数据查询/04_数据仓库层/EDW_TC主题之协同效应.sql
+++ 01_数据查询/04_数据仓库层/EDW_TC主题之协同效应.sql
@@ -36,8 +36,8 @@ with temp_fdsrc as
            where a.l_fdsrc_id = b.l_fdsrc_id(+)
              and a.l_proj_id = c.l_proj_id
              and a.l_fdsrc_inst_id = d.l_inst_id(+)
-             and a.l_month_id = 201701 --and c.c_proj_code = 'G144'
-			 ) t),
+             and a.l_month_id = 201703 --and c.c_proj_code = 'G144'
+             ) t),
 temp_actual_year as
  (select b.c_proj_code, sum(a.f_actual_tot) as f_actual_year
     from tt_sr_tstrev_proj_m a, dim_pb_project_basic b
@@ -45,12 +45,15 @@ temp_actual_year as
      and a.l_month_id >= substr(b.l_effective_date, 1, 6)
      and a.l_month_id < substr(b.l_expiration_date, 1, 6)
      and substr(a.l_month_id, 1, 4) = 2017
-     and a.l_month_Id <= 201701 --and b.c_proj_code = 'E701'
+     and a.l_month_Id <= 201703 --and b.c_proj_code = 'E701'
    group by b.c_proj_code, b.c_proj_name)
 select d.c_fdsrc_name,
        c.c_inst_name,
        a.c_proj_code,
        a.c_proj_name,
+       b.c_proj_type_n,
+       b.c_func_type_n,
+       b.c_affair_props_n,
        sum(e.f_actual_year) as f_actual_year
   from dim_pb_project_basic a,
        dim_pb_project_biz   b,
@@ -65,6 +68,8 @@ select d.c_fdsrc_name,
    and d.l_rn = 1
    and a.c_proj_code = e.c_proj_code
 --and a.c_proj_code = 'E701'
- group by d.c_fdsrc_name, c.c_inst_name, a.c_proj_code, a.c_proj_name
+ group by d.c_fdsrc_name, c.c_inst_name, a.c_proj_code, a.c_proj_name,b.c_proj_type_n,
+       b.c_func_type_n,
+       b.c_affair_props_n
  order by d.c_fdsrc_name, c.c_inst_name, a.c_proj_code, a.c_proj_name;
 
