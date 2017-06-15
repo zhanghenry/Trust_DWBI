c9b7121fb9e89116e7ebced2d9b6be1515deb6e6
zhanghenry <tianfeng.zhang@outlook.com>
2017-06-15 17:05:50 +0800

    增加循环类项目期次数时点规模
    增加循环类项目期次数时点规模

--- 01_数据查询/04_数据仓库层/EDW_TC主题之信托规模.sql
+++ 01_数据查询/04_数据仓库层/EDW_TC主题之信托规模.sql
@@ -13,3 +13,48 @@ select c.c_proj_type_n, round(sum(a.f_balance_agg) / 100000000, 2)
       --and nvl(b.l_expiry_date, 20991231) > 20160930
    and a.l_month_id = 201609
  group by c.c_proj_type_n;
+ 
+--循环类项目期次数时点规模
+select c_proj_code, c_proj_name, l_period, sum(f_scale)
+  from (select c.c_proj_code,
+               c.c_proj_name,
+               b.l_period,
+               a.f_scale as f_scale
+          from tt_tc_scale_flow_d   a,
+               dim_tc_contract      b,
+               dim_pb_project_basic c,
+               dim_pb_project_biz   d
+         where a.l_cont_id = b.l_cont_id
+           and b.l_proj_id = c.l_proj_id
+           and c.l_proj_id = d.l_proj_id
+           and d.l_cycle_flag = 1
+           and a.l_scatype_id in (3, 1, 6)
+           and a.l_change_date <= 20170331
+        --and b.l_proj_id = 524-- and b.l_period= 25
+        union all
+        select c.c_proj_code,
+               c.c_proj_name,
+               case
+                 when b.l_period is null then
+                  (select tt.l_period
+                     from dim_tc_contract tt
+                    where tt.c_cont_code = b.c_linked_cont_code
+                      and tt.l_alltransfer_flag = 1)
+                 else
+                  b.l_period
+               end as l_period,
+               a.f_scale
+          from tt_tc_scale_flow_d   a,
+               dim_tc_contract      b,
+               dim_pb_project_basic c,
+               dim_pb_project_biz   d
+         where a.l_cont_id = b.l_cont_id
+           and b.l_proj_id = c.l_proj_id
+           and c.l_proj_id = d.l_proj_id
+           and d.l_cycle_flag = 1
+           and a.l_scatype_id in (2, 7)
+           and a.l_change_date <= 20170331
+        --and b.l_proj_id = 524 --and b.l_period= 25
+        )
+ group by c_proj_code, c_proj_name, l_period
+ order by c_proj_code, c_proj_name, l_period;
