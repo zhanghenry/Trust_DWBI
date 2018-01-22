4b17f6335c35fbf1ff39a9660c32eb7485dc68f1
zhanghenry <tianfeng.zhang@outlook.com>
2018-01-22 13:48:22 +0800

    删除无用脚本

--- 01_数据查询/04_数据仓库层/EDW_MSXT_升级数据核对.sql
+++
@@ -1,7 +0,0 @@
---期次收入
-select a.c_stage_code,
-       sum(decode(a.c_revtype_code,'XTGDBC', a.f_revenue, 0)),
-       sum(decode(a.c_revtype_code,'XTFDBC', a.f_revenue, 0))
-  from tde_revenue_change a
- where to_char(a.d_revenue, 'YYYY') = '2016' and a.l_actual_flag = 0
- group by a.c_stage_code;
