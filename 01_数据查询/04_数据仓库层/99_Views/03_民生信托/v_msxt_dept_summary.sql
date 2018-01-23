d18b8da1f7db8be955d20520a70863ebaf288c75
zhanghenry <tianfeng.zhang@outlook.com>
2018-01-23 20:48:39 +0800

    更新视图
    前台业务部门统计投资收入

--- 01_数据查询/04_数据仓库层/99_Views/03_民生信托/v_msxt_dept_summary.sql
+++ 01_数据查询/04_数据仓库层/99_Views/03_民生信托/v_msxt_dept_summary.sql
@@ -1,9 +1,13 @@
+/*
+20180123视图变更
+前台业务部门的投资收入也进行统计
+*/
 create or replace view v_msxt_dept_summary as
 select a.l_month_id,
        a.l_object_id as l_dept_id,
-       case when substr(b.l_setup_date,1,4) = substr(a.l_month_id,1,4) then 'NEW' else 'EXIST' end as c_status,
+       case when nvl(substr(b.l_setup_date,1,4),substr(f.l_setup_date,1,4)) = substr(a.l_month_id,1,4) then 'NEW' else 'EXIST' end as c_status,
        sum(case
-            when e.c_dept_type = '0' and c.c_ietype_code_l1 = 'XTSR' then a.f_planned_eoy
+            when e.c_dept_type = '0' and c.c_ietype_code_l1 in( 'XTSR','TZSR') then a.f_planned_eoy
             when e.c_dept_type = '0' and c.c_ietype_code = 'TZZYFX' and a.l_stage_id <> 0 then a.f_planned_eoy*-1
             when e.c_dept_type = '1' and c.c_ietype_code_l1 = 'TZSR' then a.f_planned_eoy
             when e.c_dept_type = '2' and c.c_ietype_code_l2 = 'XTYXFY' then a.f_planned_eoy
@@ -20,9 +24,11 @@ select a.l_month_id,
    and a.l_iestatus_id = d.l_iestatus_id
    and a.l_object_id = e.l_dept_id
    and a.l_proj_id = f.l_proj_id
-   and substr(b.l_setup_date,1,6) <= a.l_month_id
+   and nvl(substr(b.l_setup_date,1,6),substr(f.l_setup_date,1,6)) <= a.l_month_id
    and d.l_recog_flag = 0
    and a.c_object_type = 'BM'
+   /*and e.c_dept_code_l1 = '0_ms16'
+   and a.l_month_id = 201712*/
    and substr(f.l_effective_date,1,6) <= a.l_month_id
    and substr(f.l_expiration_date,1,6) > a.l_month_id
-   group by a.l_month_id,a.l_object_id,case when substr(b.l_setup_date,1,4) = substr(a.l_month_id,1,4) then 'NEW' else 'EXIST' end;
+   group by a.l_month_id,a.l_object_id,case when nvl(substr(b.l_setup_date,1,4),substr(f.l_setup_date,1,4)) = substr(a.l_month_id,1,4) then 'NEW' else 'EXIST' end;
