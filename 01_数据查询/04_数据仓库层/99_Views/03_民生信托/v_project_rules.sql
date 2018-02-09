86219f0554caa2519736555405ca6f3402a191f3
zhanghenry <tianfeng.zhang@outlook.com>
2018-02-09 17:25:37 +0800

    增加信托经理B角
    增加信托经理B角

--- 01_数据查询/04_数据仓库层/99_Views/03_民生信托/v_project_rules.sql
+++ 01_数据查询/04_数据仓库层/99_Views/03_民生信托/v_project_rules.sql
@@ -1,3 +1,9 @@
+/*
+Date:20180209
+Edit By zhangtf10208
+增加信托经理B角
+*/
+
 create or replace view v_project_rules as
 with temp_emp as
  (select t.l_emp_id, t.c_emp_name from dim_pb_employee t)
@@ -7,17 +13,20 @@ select d1.l_proj_id,
        d4.c_emp_name as l_invmgr_name,
        d5.c_emp_name as l_tstacct_name,
        d6.c_emp_name as l_loanclerk_name,
-       d7.c_emp_name as l_invclerk_name
+       d7.c_emp_name as l_invclerk_name,
+       d8.c_emp_name as l_tstacct_b_name
   from dim_pb_project_basic d1,
        temp_emp            d2,
        temp_emp            d3,
        temp_emp            d4,
        temp_emp            d5,
        temp_emp            d6,
-       temp_emp            d7
+       temp_emp            d7,
+       temp_emp            d8
  where d1.l_tstmgr_id = d2.l_emp_id(+)
    and d1.l_opmgr_id = d3.l_emp_id(+)
    and d1.l_invmgr_id = d4.l_emp_id(+)
    and d1.l_tstacct_id = d5.l_emp_id(+)
    and d1.l_loanclerk_id = d6.l_emp_id(+)
-   and d1.l_invclerk_id = d7.l_emp_id(+);
+   and d1.l_invclerk_id = d7.l_emp_id(+)
+   and d1.l_tstacct_b_id = d8.l_emp_id(+);
